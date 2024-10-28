48643837e6c51e83daf8b81b4c94f1d8ed544420
송근웅 <rmsdndchlrh@naver.com>
2024-10-28 13:41:42 +0900

    Feat/#1: 프론트 초기 환경설정 추가

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,7 +1,58 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'My PWA App',
+        short_name: 'PWA App',
+        description: 'A simple PWA using Vite, React, and TypeScript',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/favicon/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/favicon/android-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/favicon/apple-icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/favicon/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/favicon/favicon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/favicon/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/favicon/ms-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
