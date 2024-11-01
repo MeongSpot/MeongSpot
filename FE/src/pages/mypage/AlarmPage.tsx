311fa7a96324abd3bf704756d0cf7270f86b779b
송근웅 <rmsdndchlrh@naver.com>
2024-11-01 17:49:24 +0900

    Fix/#2: 채팅 페이지 디자인 수정

--- FE/src/pages/mypage/AlarmPage.tsx
+++ FE/src/pages/mypage/AlarmPage.tsx
@@ -7,11 +7,6 @@ const AlarmPage = () => {
   const hasData = false;
 
   return (
-<<<<<<< HEAD:FE/src/pages/AlarmPage.tsx
-    <div className="p-4">
-      <h1 className="text-center text-lg font-bold mb-4">알림</h1>
-      <hr className="my-4 -mx-4 w-screen" />
-=======
     <div className="">
       <div className="p-4">
         <div className="grid grid-cols-3 items-center">
@@ -20,7 +15,6 @@ const AlarmPage = () => {
         </div>
       </div>
       <hr />
->>>>>>> ab4ef4fc101148b1e31372cf8014baca739cbc84:FE/src/pages/mypage/AlarmPage.tsx
       <div className="min-h-screen flex flex-col items-center justify-center mt-[-10vh]">
         {hasData ? (
           <div className="flex justify-between items-center mb-4">{/* 알림 내용 */}</div>
