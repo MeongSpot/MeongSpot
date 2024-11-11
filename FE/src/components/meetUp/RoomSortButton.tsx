d79bca6b18c7d6efa2034726285e1a88cb45d63a
박주영 <pjy7244@gmail.com>
2024-11-11 11:02:01 +0900

    Feat/#49: 전체 모임 목록 로딩상태관리 추가

--- FE/src/components/meetUp/RoomSortButton.tsx
+++ FE/src/components/meetUp/RoomSortButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 interface SortButtonsProps {
-  sortBy: string;
+  sortBy: 'recent' | 'remain';
   onSortChange: (sortType: string) => void;
 }
 
@@ -9,13 +9,13 @@ const RoomSortButton: React.FC<SortButtonsProps> = ({ sortBy, onSortChange }) =>
   <div className="flex space-x-2">
     <button
       onClick={() => onSortChange('latest')}
-      className={`px-3 py-1 rounded-full ${sortBy === 'latest' ? 'bg-cream-bg border-deep-coral' : 'bg-white'} border `}
+      className={`px-3 py-1 rounded-full ${sortBy === 'recent' ? 'bg-cream-bg border-deep-coral' : 'bg-white'} border `}
     >
       최신순
     </button>
     <button
       onClick={() => onSortChange('oldest')}
-      className={`px-3 py-1 rounded-full ${sortBy === 'oldest' ? 'bg-cream-bg border-deep-coral' : 'bg-white'} border`}
+      className={`px-3 py-1 rounded-full ${sortBy === 'remain' ? 'bg-cream-bg border-deep-coral' : 'bg-white'} border`}
     >
       남은 시간순
     </button>
