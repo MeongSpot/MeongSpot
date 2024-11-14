42a2b06370c6d0dafc7f04781dbece56ba43897f
송근웅 <rmsdndchlrh@naver.com>
2024-11-15 00:25:39 +0900

    Fix/#2: 실시간 채팅 수정

--- FE/src/pages/chat/GroupChatPage.tsx
+++ FE/src/pages/chat/GroupChatPage.tsx
@@ -23,7 +23,6 @@ const GroupChatPage = () => {
   const location = useLocation();
   const animateBack = location.state?.animateBack ?? true;
 
-  // isGroupChat을 true로 설정하여 그룹 채팅에 대해 입장 메시지 적용
   const { messages: fetchedMessages, loading, error, myId } = useChatDetail(Number(roomId), page);
   const { sendMessage } = useChat(Number(roomId), nickname);
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
